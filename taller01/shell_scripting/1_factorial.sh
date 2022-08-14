38f62d0ccaf7b0d23f1a69eea06f6c13089b2a14
Jason Nicolas Arias <jn.arias@uniandes.edu.co>
2022-08-14 15:12:16 -0500

    changed input type of factorial script

--- taller01/shell_scripting/1_factorial.sh
+++ taller01/shell_scripting/1_factorial.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-read -p "Enter a number : " n
-fact=1
-for (( i=1; i<=n; i++ ))
-do
-    fact=$(( $fact * $i ))
-done
-echo "Factorial of $n is $fact"
+if [ $# -eq 1 ]; then
+    fact=1
+    for ((i = 1; i <= $1; i++)); do
+        fact=$(($fact * $i))
+    done
+    echo "Factorial of $1 is $fact"
+else
+    echo "debe ingresar solo un parametro"
+fi
