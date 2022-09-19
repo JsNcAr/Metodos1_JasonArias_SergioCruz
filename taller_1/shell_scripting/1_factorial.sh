46cbdd017c059e6197795018ec3050c97029b062
JsNcAr <jn.arias@uniandes.edu.co>
2022-09-19 07:40:13 -0500

    updated folder

---
+++ taller_1/shell_scripting/1_factorial.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]; then
+    fact=1
+    for ((i = 1; i <= $1; i++)); do
+        fact=$(($fact * $i))
+    done
+    echo $fact
+else
+    echo "debe ingresar solo un parametro"
+fi
---
+++ taller_1/shell_scripting/6_BigO.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo 'Big O complexity of 1_factorial is O(n**2)'
